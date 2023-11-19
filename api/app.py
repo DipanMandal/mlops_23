394bd675ce1c3b38232d10c702e1ccea0b3adff8
DipanMandal <dipan@DESKTOP-SQV5REV>
2023-11-19 16:42:50 +0530

    quiz 4

--- api/app.py
+++ api/app.py
@@ -1,16 +1,27 @@
-from flask import Flask, request 
+import matplotlib.pyplot as plt
+import sys
+from sklearn import datasets, metrics, svm
+from sklearn.model_selection import train_test_split
+import pdb
+from joblib import dump,load
+import numpy as np
+import pandas as pd
+from flask import Flask, request, jsonify
+from PIL import Image
+import numpy as np
 
 app = Flask(__name__)
 
-@app.route("/hello/<val>")
-def hello_world(val):
-    return "<p>Hello, World!</p>" + val 
+model = load('./models/svm_gamma:0.01_C:1.joblib')
+
+@app.route('/predict', methods=['POST'])
+def pred_model():
+    data = request.get_json()
+    image = data['image']
+    pred1 = model.predict(image)
+    #reurn pred1 in json
+    return jsonify(prediction=pred1.tolist())
 
 
-@app.route("/model", methods=['POST'])
-def digit_model():
-    js = request.get_json()
-    x = js['x']
-    y = js['y']
-
-    return x+y
+if __name__ == '__main__':
+    app.run()
